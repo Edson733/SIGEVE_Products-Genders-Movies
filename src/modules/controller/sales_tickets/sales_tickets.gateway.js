34d6940a6c5769d01ead8386010186825de64ab3
Edson733 <20213tn034@utez.edu.mx>
2022-12-06 23:45:31 -0600

    Algunos cambiecitos pero ya listo todo

--- src/modules/controller/sales_tickets/sales_tickets.gateway.js
+++ src/modules/controller/sales_tickets/sales_tickets.gateway.js
@@ -1,28 +1,28 @@
 const {query} = require("../../../utils/mysql");
 
 const findAll = async () => {
-    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, clients.name_cet, 
+    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, users.name_usr, 
                     sales_tickets.total_count, sales_tickets.status_ste FROM sales_tickets 
-                    JOIN movie_shows JOIN clients ON sales_tickets.movie_show_ste = movie_shows.id_msw 
-                    && sales_tickets.client_spo = clients.id_cet;`;
+                    JOIN movie_shows JOIN users ON sales_tickets.movie_show_ste = movie_shows.id_msw 
+                    && sales_tickets.client_spo = users.id_usr;`;
     return await query(sql, []);
 };
 
 const findEnable = async () => {
-    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, clients.name_cet, 
+    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, users.name_usr, 
                     sales_tickets.total_count, sales_tickets.status_ste FROM sales_tickets 
-                    JOIN movie_shows JOIN clients ON sales_tickets.movie_show_ste = movie_shows.id_msw 
-                    && sales_tickets.client_spo = clients.id_cet WHERE status_ste = 1;`;
+                    JOIN movie_shows JOIN users ON sales_tickets.movie_show_ste = movie_shows.id_msw 
+                    && sales_tickets.client_spo = users.id_usr WHERE status_ste = 1;`;
     return await query(sql, []);
 };
 
 const findById = async (id) => {
     if (Number.isNaN(id)) throw Error('Wrong type');
     if (!id) throw Error('Missing fields');
-    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, clients.name_cet, 
+    const sql = `SELECT sales_tickets.id_ste, sales_tickets.movie_show_ste, users.name_usr, 
                     sales_tickets.total_count, sales_tickets.status_ste FROM sales_tickets 
-                    JOIN movie_shows JOIN clients ON sales_tickets.movie_show_ste = movie_shows.id_msw 
-                    && sales_tickets.client_spo = clients.id_cet WHERE id_ste = ?;`;
+                    JOIN movie_shows JOIN users ON sales_tickets.movie_show_ste = movie_shows.id_msw 
+                    && sales_tickets.client_spo = users.id_usr WHERE id_ste = ?;`;
     return await query(sql, [id]);
 };
 
